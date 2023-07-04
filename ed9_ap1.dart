b539a4c242f1951f04a8fa9e59b171863f74928f
DevRogerio <130984908+DevRogerio@users.noreply.github.com>
2023-07-04 15:42:48 -0300

    ed9_ap1.dart

---
+++ ed9_ap1.dart
@@ -0,0 +1,32 @@
+# academy_pratica_e9
+
+import 'dart:async';
+
+Future<String> consultarAPI(String codigoRastreamento) async {
+  await Future.delayed(Duration(seconds: 2));
+
+  if (codigoRastreamento == 'ABC123') {
+    return 'Em trânsito';
+  } else if (codigoRastreamento == 'XYZ789') {
+    return 'Entregue';
+  } else if (codigoRastreamento == 'DEF456') {
+    return 'Atrasado';
+  } else {
+    return 'Código de rastreamento inválido';
+  }
+}
+
+Future<void> verificarStatusEntrega(String codigoRastreamento) async {
+  String status = await consultarAPI(codigoRastreamento);
+  print('Status de entrega do pacote $codigoRastreamento: $status');
+}
+
+void main() async {
+  print('Início');
+  await Future.wait([
+    verificarStatusEntrega('ABC123'),
+    verificarStatusEntrega('XYZ789'),
+    verificarStatusEntrega('DEF456'),
+  ]);
+  print('Fim');
+}
